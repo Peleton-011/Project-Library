51c428a2c2da95c9da7b161ec1167e9d4b63485a
Nico <nicodiazan@gmail.com>
2022-10-13 20:48:08 +0200

    Add addBookToLibrary() function, to add a new book to the books array

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const Book = function (title, author, pageLen, isRead) {
+const books = [];
+
+const Book = function (title, author, pageLen, isRead, coverImg) {
     this.title = String(title),
     this.author = String(author),
     this.pageLen = Number(pageLen),
@@ -7,4 +9,10 @@ const Book = function (title, author, pageLen, isRead) {
         let readStatus = this.isRead ? "already read" : "not read yet";
         return `${this.title} by ${this.author}, ${this.pageLen} pages, ${readStatus}`;
     }
+    this.coverImg = String(coverImg) || null;
 }
+
+function addBookToLibrary (title, author, pageLen, isRead, coverImg) {
+    books.push(new Book(title, author, pageLen, isRead, coverImg));
+}
+
