ce6ed33abe20b4fc737c4a90a72bc8ccd606946e
Nico <nicodiazan@gmail.com>
2022-10-14 22:40:33 +0200

    Reformat code for readability and consistency

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 const books = [];
 
-const Book = function (title, author, pageLen, isRead, coverImg, description, id) {
-    this.title = String(title),
-    this.author = String(author),
-    this.pageLen = Number(pageLen),
-    this.isRead = Boolean(isRead),
-    this.info = () => {
-        let readStatus = this.isRead ? "already read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pageLen} pages, ${readStatus}`;
-    }
+const Book = function (
+    title,
+    author,
+    pageLen,
+    isRead,
+    coverImg,
+    description,
+    id
+) {
+    (this.title = String(title)),
+        (this.author = String(author)),
+        (this.pageLen = Number(pageLen)),
+        (this.isRead = Boolean(isRead)),
+        (this.info = () => {
+            let readStatus = this.isRead ? "already read" : "not read yet";
+            return `${this.title} by ${this.author}, ${this.pageLen} pages, ${readStatus}`;
+        });
     this.coverImg = String(coverImg) || null;
     this.description = String(description) || null;
     this.id = id || newId();
-}
+};
 
-function addBookToLibrary (title, author, pageLen, isRead, coverImg) {
+function addBookToLibrary(title, author, pageLen, isRead, coverImg) {
     books.push(new Book(title, author, pageLen, isRead, coverImg));
 }
 
-function displayBooks () {
+function displayBooks() {
     let html = "";
     for (let i = 0; i < books.length; i++) {
         let book = books[i];
-        let desc = book.description ? `<p class="book-description">${book.description}</p>` : "c"; 
+        let desc = book.description
+            ? `<p class="book-description">${book.description}</p>`
+            : "c";
         html += `
-            <div class="book" id="${book.id}" style="background-image: ${book.coverImg}">
+            <div class="book" id="${book.id}" style="background-image: ${
+            book.coverImg
+        }">
             <div class="buttons">
-                <button class="book-isRead">${book.isRead ? "already read" : "not read yet"}</button>
+                <button class="book-isRead">${
+                    book.isRead ? "already read" : "not read yet"
+                }</button>
                 <button class="book-delete">Del</button>
             </div>
             <div class="book-info">
@@ -39,7 +53,10 @@ function displayBooks () {
     }
     //Add to dom
     document.getElementById("book-list").innerHTML = html;
+}
 
+function newId() {
+    return books[books.length - 1] ? books[books.length - 1].id + 1 : 0;
 }
 
 //Add sample books to library
@@ -55,11 +72,6 @@ function sampleBooks(amt) {
         );
     }
 }
-
-function newId () {
-    return books[books.length - 1] ? books[books.length - 1].id + 1 : 0; 
-}
-
 
 //Add sample books to library
 sampleBooks(10);
