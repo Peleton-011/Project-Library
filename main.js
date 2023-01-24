3119a1bc3d02bfe394db4e8f0bdcabd4ba89f0ee
Nico <nicodiazan@gmail.com>
2023-01-24 20:10:25 +0100

    Fix class field declarations

--- main.js
+++ main.js
@@ -12,20 +12,20 @@ class Book {
         description,
         id
     ) {
-        const title = typeof (title) !== "string" ? "Untitled" : title;
-        const author = typeof (author) !== "string" ? "Anonymous" : author;
-        const pageLen = typeof (pageLen) !== "number" ? null : pageLen;
-        let isRead = typeof (isRead) !== "boolean" ? false : isRead;
-        const info = () => {
-            let readStatus = isRead ? "already read" : "not read yet";
+        this.title = typeof (title) !== "string" ? "Untitled" : title;
+        this.author = typeof (author) !== "string" ? "Anonymous" : author;
+        this.pageLen = typeof (pageLen) !== "number" ? null : pageLen;
+        this.isRead = typeof (isRead) !== "boolean" ? false : isRead;
+        this.info = () => {
+            let readStatus = this.isRead ? "already read" : "not read yet";
             return `${this.title} by ${this.author}, ${this.pageLen} pages, ${readStatus}`;
         };
-        const coverImg = typeof (coverImg) !== "string" ? "" : coverImg;
-        const description =
+        this.coverImg = typeof (coverImg) !== "string" ? "" : coverImg;
+        this.description =
         typeof (description) !== "string"
             ? "Read to find out more!"
             : description;
-        const id = typeof (id) !== "number" ? newId() : id;
+        this.id = typeof (id) !== "number" ? newId() : id;
         };
 }
 
